fe4274bf306780cb801970fff2da1b5c0fa88b3b
mz-alee <aliraza.1006724@gmail.com>
2025-07-15 12:30:19 +0500

    latest update

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -240,12 +240,6 @@ const Login = () => {
                   console.log(error);
                 }}
               />
-              <button
-                onClick={() => signIn("facebook")}
-                className="bg-[#1877f2] text-white px-4 py-2 rounded mt-3"
-              >
-                Login with Facebook
-              </button>
             </div>
           </div>
         </div>
